72c57db1aaca62dadd2851a4a0ccc568645f5fcf
WestEast1st <sai10nhid@gn.iwasaki.ac.jp>
2018-06-06 09:40:09 +0900

    [add] javaren3 coed

---
+++ JavaRen3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class JavaRen3{
+    public static void main ( String args[] ) throws IOException {
+        BufferedReader bin = new BufferedReader( new InputStreamReader( System.in ) );
+        String msg;
+        double height , weight ;
+        System.out.println("氏名を入力してください");
+        msg = bin.readLine() + "さんの標準体重は ";
+        System.out.println(msg + "さんの身長は？");
+        height = bin.readLine();
+        weight = ( height - 100 ) * 0.9;
+        System.out.println( msg + height + "kgです");
+
+    }
+}
