1063c209f319c211e7ee4c435a9a1727ff0162f8
WestEast1st <sai10nhid@gn.iwasaki.ac.jp>
2018-06-06 10:41:15 +0900

    [a]

---
+++ JavaRen4.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+class JavaRen4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bin = new BufferedReader(new InputStreamReader(System.in));
+        String input_string;
+        int input_size , outer_i , inner_i , wide , Center;
+
+        System.out.println("全角文字の入力");
+        input_string    = bin.readLine();
+
+        System.out.println("サイズの入力");
+        input_size      = Integer.parseInt( bin.readLine() );
+        wide            = input_size * 2 - 1;
+        Center          = Math.round(wide/2);
+        for(outer_i=0;outer_i<input_size;outer_i++){
+            String buf="0";
+            for(inner_i=1;inner_i<wide;inner_i++){
+                if(inner_i == Center+outer_i ){
+                    buf += input_string;
+                }else{
+                    buf += inner_i;
+                }
+            }
+            System.out.println(buf);
+        }
+    }
+}
