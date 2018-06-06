71710138d598a11f8583910bc19eed486845dcf6
WestEast1st <sai10nhid@gn.iwasaki.ac.jp>
2018-06-06 09:46:14 +0900

    [fix] java code

--- JavaRen3.java
+++ JavaRen3.java
@@ -8,9 +8,8 @@ class JavaRen3{
         System.out.println("氏名を入力してください");
         msg = bin.readLine() + "さんの標準体重は ";
         System.out.println(msg + "さんの身長は？");
-        height = bin.readLine();
+        height = Integer.oarseInt( bin.readLine() );
         weight = ( height - 100 ) * 0.9;
         System.out.println( msg + height + "kgです");
-
     }
 }
